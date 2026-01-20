58be4bade4b6a30c05ecbddb0be8ad6ff6bf0010
kihoo56tkr <161791642+kihoo56tkr@users.noreply.github.com>
2026-01-20 17:28:55 +0800

    Level-0: Rename chatbot to MIntel and add greeting

---
+++ src/main/java/Mintel.java
@@ -0,0 +1,15 @@
+public class Mintel {
+    public static void main(String[] args) {
+        String logo = "  /\\_/\\  \n" +
+                " ( o.o ) \n" +
+                "  > ^ <  \n" +
+                " MINTEL";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n" +
+                        "Hello! I'm MIntel\n" +
+                        "What can I do for you?\n" +
+                        "____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________");
+    }
+}
